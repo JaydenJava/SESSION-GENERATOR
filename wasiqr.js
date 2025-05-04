93b9981bce84a4d10262a8ef8113607a17d4742d
Jayden Java <jaydenkmb56@gmail.com>
2025-05-04 22:44:51 +0300

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -59,22 +59,23 @@ router.get('/', async (req, res) => {
 				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
 	
 				   let WASI_MD_TEXT = `
-*_Session Connected By Wasi Tech_*
+*_Qr Code By Jayden Java_*
+*_Made With 🤍_*
+______________________________________
+*_Pair Code Connected by Jayden Java_*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『AMAZING YOU'VE CHOSEN WASI MD』*
+║ *『 WOW, THANKS FOR USING JAYDEN-MD 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
-╚════════════════════════╝
+╚══════════════════════╝
 ╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1
-║❒ *Owner:* _https://wa.me/message/THZ3I25BYZM2E1_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD_
-║❒ *WaGroup:* _https://chat.whatsapp.com/FF6YuOZTAVB6Lu65cnY5BN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j_
-║❒ *Plugins:* _https://github.com/Itxxwasi 
-╚════════════════════════╝
+║  『••• *FIND THE CREATOR VIA* •••』
+║❒ *Owner:* _https://wa.me/+256745944178_
+║❒ *Repo:* _https://github.com/JaydenJava/JAYDEN-MD_
+║❒ *Telegram:* _https://t.me/JaydenJava2
+║❒ *WaGroup:* _https://chat.whatsapp.com/EMI9FGpIQh57uHlHCRDb7D_
+╚══════════════════════╝ 
 _____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
